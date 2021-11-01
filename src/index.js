b5172b7a33540e005826cb6b18a22f83622f3b06
cheesecake001 <93205208+cheesecake001@users.noreply.github.com>
2021-11-01 22:25:30 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import Contact from './Contact';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/contact">Contact-me</Link></li>
+
+    </ul>
+    <hr />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/contact" component={Contact} />
+
+  </div>
+</Router>
+
+, document.getElementById('root'));
+registerServiceWorker();
